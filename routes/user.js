5ec767e04b4537d95383ea5240682c9e216f342a
LinMichRa <lramirezd2@ucentral.edu.co>
2024-08-28 11:30:11 -0500

    Authorization added

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { login, register, testUser } from "../controllers/user.js";
+import { ensureAuth } from "../middlewares/auth.js";
 
 const router = Router();
 
 //Definir las rutas
-router.get('/tesst-user',testUser)
+router.get('/test-user', ensureAuth, testUser)
 router.post('/register',register)
 router.post('/login',login)
 
